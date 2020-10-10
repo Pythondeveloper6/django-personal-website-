043847e98b5d876b5555b3117742afc684f4dfe6
Pythondeveloper6 <pythondeveloper6@gmail.com>
2020-10-10 21:01:23 +0200

    finish service app without client feedback

--- service/views.py
+++ service/views.py
@@ -8,3 +8,8 @@ from django.views.generic import ListView
 class ServiceList(ListView):
     model = Service
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["facts"] = Facts.objects.last() 
+        return context
+    
