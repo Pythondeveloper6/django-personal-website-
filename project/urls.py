4d0fb3059eb0171df79bd5348eb3318532378301
Pythondeveloper6 <pythondeveloper6@gmail.com>
2020-10-10 20:43:01 +0200

    add static & media fiels config

--- project/urls.py
+++ project/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.urls import path , include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('service/',include('service.urls',namespace='service')),
 ]
+
+
+urlpatterns +=  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns +=  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
